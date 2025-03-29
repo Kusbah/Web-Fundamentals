202a92b41e844de9bf95c8e4f7161b67e3ea708b
kusbah <abdkusbah2@gmail.com>
2025-03-29 15:39:26 +0200

    after code review

--- Making-Connections/script.js
+++ Making-Connections/script.js
@@ -1,39 +1,28 @@
-function gear(){
+function gear() {
     document.querySelector('.username').innerText = "Liam W";
 }
-function accept(){
-    let requestElement = document.querySelector(".user1");
-    requestElement.remove();
-    var count = document.querySelector(".countNumb")
-    let currentCount = parseInt(count.innerText);
-    count.innerText = currentCount - 1;
-    var count2 = document.querySelector(".countnumbreq")
-    var currentCount2 = parseInt(count2.innerText);
-    count2.innerText = currentCount2 + 1;
+function accept(userClass) {
+    let requestElement = document.querySelector("." + userClass);
+    if (requestElement) {
+        requestElement.remove();
+    }
 
-}
-function declined(){
-    let requestElement = document.querySelector(".user1");
-    requestElement.remove();
-    var count = document.querySelector(".countNumb")
+    let count = document.querySelector(".countNumb");
     let currentCount = parseInt(count.innerText);
     count.innerText = currentCount - 1;
-}
-function declined1(){
-    let requestElement = document.querySelector(".user2");
-    requestElement.remove();
-    var count = document.querySelector(".countNumb")
-    let currentCount = parseInt(count.innerText);
-    count.innerText = currentCount - 1;
-}
-function accept1(){
-    let requestElement = document.querySelector(".user2");
-    requestElement.remove();
-    var count = document.querySelector(".countNumb")
-    let currentCount = parseInt(count.innerText);
-    count.innerText = currentCount - 1;
-    var count2 = document.querySelector(".countnumbreq")
-    var currentCount2 = parseInt(count2.innerText);
-    count2.innerText = currentCount2 + 1;
 
+    let count2 = document.querySelector(".countnumbreq");
+    let currentCount2 = parseInt(count2.innerText);
+    count2.innerText = currentCount2 + 1;
+}
+
+function declined(userClass) {
+    let requestElement = document.querySelector("." + userClass);
+    if (requestElement) {
+        requestElement.remove();
+    }
+
+    let count = document.querySelector(".countNumb");
+    let currentCount = parseInt(count.innerText);
+    count.innerText = currentCount - 1;
 }
