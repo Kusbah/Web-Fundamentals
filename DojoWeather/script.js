aec3290dcaaf3799526013852311aa9b5292c0a6
kusbah <abdkusbah2@gmail.com>
2025-03-29 15:48:12 +0200

    edite after code review

--- DojoWeather/script.js
+++ DojoWeather/script.js
@@ -3,7 +3,6 @@ function acceptCookies() {
 }
 
 
-let isCelsius = true;
 
 function convertTemperature() {
     let tempSelect = document.querySelector("#temp").value.trim();
@@ -11,16 +10,15 @@ function convertTemperature() {
 
     degrees.forEach(degree => {
         let temp = parseFloat(degree.innerText);
-        
-            if (tempSelect === "fahrenheit" && isCelsius) {
-                temp = Math.round((temp * 9/5) + 32);
-            } else if (tempSelect === "celsius" && !isCelsius) {
-                temp = Math.round((temp - 32) * 5/9);
-            }
-            degree.innerText = temp;
-        
+
+        if (tempSelect === "fahrenheit") {
+            temp = Math.round((temp * 9 / 5) + 32);
+        } else if (tempSelect === "celsius") {
+            temp = Math.round((temp - 32) * 5 / 9);
+        }
+        degree.innerText = temp;
+
     });
-    isCelsius = tempSelect === "celsius";
 }
 
 
